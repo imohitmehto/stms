416f27d06a48a29695d77f3f2ae18ab1f7f65a7c
Mohit Mehto <imohitmehto@gmail.com>
2025-04-05 11:03:38 +0530

    feat: integrate TypeORM and PostgreSQL, add tasks module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // load environment variables from .env file
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432'), // default 5432 
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // true in development
+      // synchronize: false, // false in production
+      logging: true, // true in development 
+      // logging: false, // false in production  
+    }),
+    // TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
